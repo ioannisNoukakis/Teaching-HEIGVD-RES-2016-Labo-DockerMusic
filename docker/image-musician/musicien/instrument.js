446376d9a4a64b84ee886a7455adaf85357e00fd
Ioannis Noukakis <ioannis.noukakisheig-vd.ch>
2016-05-10 13:35:37 +0200

    for update

--- docker/image-musician/musicien/instrument.js
+++ docker/image-musician/musicien/instrument.js
@@ -1,8 +1,3 @@
-/*
- TODO: faire une exception si pas un instrument passé en paramètre
-
-*/
-
 var protocol = require('./instruments-protocol'); 
 var uuid = require('node-uuid')
 
@@ -16,27 +11,15 @@ instrumentToSound.set("flute", "trulu");
 instrumentToSound.set("violin", "gzi-gzi");
 instrumentToSound.set("drum", "boum-boum");
 
-/*
- * We use a standard Node.js module to work with UDP
- */
 var dgram = require('dgram');
 
-/*
- * Let's create a datagram socket. We will use it to send our UDP datagrams 
- */
 var s = dgram.createSocket('udp4');
 
 /*
- * Let's define a javascript class for our thermometer. The constructor accepts
- * a location, an initial temperature and the amplitude of temperature variation
- * at every iteration
- */
+class representing a musician
+*/
 function Instrument(instrument) {
 
-  /*
-   * We will simulate temperature changes on a regular basis. That is something that
-   * we implement in a class method (via the prototype)
-   */
 	Instrument.prototype.update = function() {
 	
 		var pakage = {
@@ -44,12 +27,8 @@ function Instrument(instrument) {
 			sound: instrumentToSound.get(instrument)
 		};		
 
-		var payload = JSON.stringify(pakage); 
+		var payload = JSON.stringify(pakage);
 
-/*
-	   * Finally, let's encapsulate the payload in a UDP datagram, which we publish on
-	   * the multicast address. All subscribers to this address will receive the message.
-	   */
 		message = new Buffer(payload);
 		s.send(message, 0, message.length, protocol.PROTOCOL_PORT, protocol.PROTOCOL_MULTICAST_ADDRESS, function(err, bytes) {
 			console.log("[V1]Sending payload: " + message);
@@ -61,14 +40,10 @@ function Instrument(instrument) {
 
 }
 
-/*
- * Let's get the thermometer properties from the command line attributes
- * Some error handling wouln't hurt here...
- */
-var sound = process.argv[2];
+if(process.argv.length != 2)
+	throw err;
+else
+	var sound = process.argv[2];
 
-/*
- * Let's create a new thermoter - the regular publication of measures will
- * be initiated within the constructor
- */
+
 var t1 = new Instrument(sound);
